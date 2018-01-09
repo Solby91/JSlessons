bd9c026dd5565ec5242fbf0a4a55a35f3d0cdf3e
Solby91 <solby91@gmail.com>
2018-01-09 22:55:44 +0300

    first lesson, part 3

--- lesson-1/scripts/hello-world.js
+++ lesson-1/scripts/hello-world.js
@@ -67,3 +67,72 @@ outer: for(var simpleNum = 2; simpleNum<11; simpleNum++){
     alert('Число ' + simpleNum + ' простое');
 }
 */  // проверка целого числа на "простое"
+
+/*
+function pow(x, n) {
+    x = +prompt('Введите число для вычисления его значения в степени', '');
+    n = +prompt('Введите степень', '');
+    if (n==0){
+        return alert('Результат выражения: 1');
+    } else if (n==1) {
+        return alert('Результат выражения: ' + x);
+    }
+    var pownum = x;
+    while (n>1) {
+        pownum *=x;
+        n--;
+    }
+    alert('Результат выражения: ' + pownum);
+}
+*/  // возведение в степень
+
+/*
+function sumWhile(n) {
+    var sum = 0;
+    while(n>0){
+        sum += n;
+        n--;
+    }
+    alert(sum);
+}
+*/  // рекурсия через цикл
+
+/*
+function sumTo(n) {
+    if (n==1) return 1;
+        return n+sumTo(n-1);
+}
+*/  // сумма цифр через рекурсию
+
+/*
+function factorial(n) {
+    if (n==1) return n;
+    return n*factorial(n-1);
+}
+*/  // факториал через рекурсию
+
+/*
+function fib(n) {
+   if (n<=1) {
+        return n;
+   } else {
+       return fib(n-1)+fib(n-2);
+   }
+
+}
+*/  // числа Фибоначи через рекурсию
+
+/*
+function fibCycle(n) {
+    var a = 1;
+    var b = 1;
+    var c = 0;
+
+    for (var i=3; i<=n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
+*/  // числа Фибоначи через цикл
